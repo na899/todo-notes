bca21eb7b709e168324014cdaf631ee7cc5e7639
na899 <pnaveena1999@gmail.com>
2018-06-19 20:52:21 +0530

    session not working

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,47 +1,53 @@
-module.exports=function(app){
-var bcrypt=require('bcrypt');
+module.exports = function(app) {
+        var bcrypt = require('bcrypt');
 
 
 
-var bodyParser= require('body-parser');
+        var bodyParser = require('body-parser');
 
-const MongoClient = require('mongodb').MongoClient
-
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-	app.get('/login',(req,res)=>{
-
-     res.render('login.ejs');
-	
-});
+        const MongoClient = require('mongodb').MongoClient
 
 
-	app.post('/', (req, res) => {
-	var item=req.body;
-    var check=0;
-    db.collection("users").find({username:item.username}).toArray(function(err, result) {
-          if (err) throw err;
-          check=result[0];
-          console.log(req.session);
-         if(check!=0)
-     {
-     if(bcrypt.compareSync(item.password, check.pass)) {
-     	userID=item.username;
-     	console.log(userID)
-         db.collection('todos'+userID).find().toArray(function(err, results) {
+        app.use(bodyParser.urlencoded({
+                extended: true
+        }));
+        app.use(bodyParser.json());
 
-         res.render('todo.ejs',{todos:results})
-    })    
-} else {
-	res.send("Wrong Credentials!")
- // Passwords don't match
-}
-    }
-    
-  });
+        app.get('/login', (req, res) => {
 
-})
+                res.render('login.ejs');
+
+        });
+
+
+        app.post('/', (req, res) => {
+                var item = req.body;
+                var check = 0;
+                db.collection("users").find({
+                        username: item.username
+                }).toArray(function(err, result) {
+                        if (err) throw err;
+                        check = result[0];
+                        console.log(req.session);
+                        if (check != 0) {
+                                if (bcrypt.compareSync(item.password, check.pass)) {
+                                        userID = item.username;
+                                        console.log(userID)
+                                        db.collection('todos' + userID).find().toArray(function(err, results) {
+
+                                                res.render('todo.ejs', {
+                                                        todos: results
+                                                })
+                                        })
+                                }
+                                else {
+                                        res.send("Wrong Credentials!")
+                                        // Passwords don't match
+                                }
+                        }
+
+                });
+
+        })
 
 }
